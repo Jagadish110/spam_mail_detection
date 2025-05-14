744ed7f9f479b21e507c9992016d85e3e50657e4
Jagadish110 <nallupureddyjagadishwarreddy@gmail.com>
2025-05-15 00:07:12 +0530

    first commit

--- backend.py
+++ backend.py
@@ -1,24 +1,40 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI,Request,Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 import joblib
-
+from pathlib import Path
+import numpy as np
 
 app=FastAPI()
 
-class model_inpit(BaseModel):
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*',]
+)
+#load the model
+model_path=Path(__file__).parent/'spam_mail_classifier.pkl'
+model=joblib.load(model_path)
 
-    email_text:str
+templates= Jinja2Templates(directory="templates")
 
-loaded_model=joblib.load(open('models/spam_model.pkl','rb'))
+@app.get("/",response_class=HTMLResponse)
+async def root(request : Request):
+    return templates.TemplateResponse("index.html",{"request": request})
+@app.post("/predict",response_class=HTMLResponse)
+async def predict(
+    request: Request,
+    email_text: str=Form(...)
+):
+    input_data=np.array([[email_text]])
+    prediction=model.predict(input_data)[0]
+    result="The given mail is spam " if prediction==1 else "it is not spam mail"
 
-@app.get('/')
-def read_root():
-    return {"message":"model loaded succesfully"}
-@app.post("/predict")
-def predict(data:model_inpit):
-    input_array=[ data.email_text ]
-    prediction=loaded_model.predict(input_array)
-    return {"prediction":int(prediction[0])}
-
-    
-    
+    return templates.TemplateResponse("index.html",{
+        "request":request,
+        "result":result
+                                      
+  })
